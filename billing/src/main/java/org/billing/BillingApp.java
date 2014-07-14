8e246a5f8547d2d117b1d17328dc8cabb97eea4b
Jeffrey Williams <jeffxor.williams@gmail.com>
2014-07-13 22:03:42 -0500

    Move out infrastructure code for amqp and axon saga repos into a seperate project. Add an empty saga to the Billing project for Refund Policy.

--- billing/src/main/java/org/billing/BillingApp.java
+++ billing/src/main/java/org/billing/BillingApp.java
@@ -1,7 +1,9 @@
 package org.billing;
 
-import org.axonframework.commandhandling.CommandBus;
-import org.axonframework.commandhandling.GenericCommandMessage;
+import java.util.Scanner;
+
+import org.axonframework.domain.GenericEventMessage;
+import org.axonframework.eventhandling.EventBus;
 import org.billing.events.OrderBilled;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -11,13 +13,16 @@ public class BillingApp
     public static void main( String[] args )
     {
     	ApplicationContext context =
-    		    new ClassPathXmlApplicationContext(new String[] {"crm.xml"});
-    	CommandBus commandBus = context.getBean(CommandBus.class);
+    		    new ClassPathXmlApplicationContext(new String[] {"billing.xml", "axon-eventbus.xml", 
+    		    		"amqp-config.xml", "persistence-infrastructure-context.xml"});
+    	EventBus eventBus = context.getBean(EventBus.class);
+    	
+    	Scanner scanner = new Scanner(System.in);
         while (true)
         {
             System.out.println("Enter customer ID:");
-            String customerId = System.console().readLine();
-            commandBus.dispatch(new GenericCommandMessage<OrderBilled>(new OrderBilled(customerId)));            
+            String customerId = scanner.nextLine();
+            eventBus.publish(new GenericEventMessage<OrderBilled>(new OrderBilled(customerId)));
         }
     }
 }
---
+++ billing/src/main/java/org/billing/saga/RefundPolicySaga.java
@@ -0,0 +1,8 @@
+package org.billing.saga;
+
+import org.axonframework.saga.annotation.AbstractAnnotatedSaga;
+
+@SuppressWarnings("serial")
+public class RefundPolicySaga extends AbstractAnnotatedSaga {
+
+}
